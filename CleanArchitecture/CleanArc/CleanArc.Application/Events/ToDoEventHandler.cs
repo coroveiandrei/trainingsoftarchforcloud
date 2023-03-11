a069b52035835ebe21c11b882c2a31d29d7ab7aa
Andrei Corovei <andrei.corovei@qubiz.com>
2023-03-11 10:37:27 +0200

    added notification service

--- CleanArchitecture/CleanArc/CleanArc.Application/Events/ToDoEventHandler.cs
+++ CleanArchitecture/CleanArc/CleanArc.Application/Events/ToDoEventHandler.cs
@@ -2,12 +2,18 @@
 using CleanArc.Domain.Events;
 using System.Threading;
 using System.Threading.Tasks;
+using CleanArc.Application.Services;
 
 namespace CleanArc.Application.Events
 {
     public class ToDoEventHandler : INotificationHandler<ToDoAdded>
     {
-        
+        private readonly INotificationService _notificationService;
+        public ToDoEventHandler(INotificationService notificationService)
+        {
+            _notificationService = notificationService;
+        }
+
         public ToDoEventHandler()
         {
             
---
+++ CleanArchitecture/CleanArc/CleanArc.Application/Services/NotificationService.cs
@@ -0,0 +1,28 @@
+﻿using CleanArc.Common;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CleanArc.Application.Services
+{
+    [MapServiceDependency(nameof(INotificationService))]
+    public class NotificationService : INotificationService
+    {
+        public async Task NotifyAsync(string message)
+        {
+            var json = JsonConvert.SerializeObject(message);
+            var data = new StringContent(json, Encoding.UTF8, "application/json");
+
+            var url = "https://httpbin.org/post";
+            using var client = new HttpClient();
+
+            var response = await client.PostAsync(url, data);
+
+            var result = await response.Content.ReadAsStringAsync();
+        }
+    }
+}
